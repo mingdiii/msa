fda2c424f7a02be0c2649dbb35721949b1e2e7e8
mingdiii <64004683+mingdiii@users.noreply.github.com>
2020-06-26 17:16:16 +0900

    Update k8s.sh

--- k8s/k8s.sh
+++ k8s/k8s.sh
@@ -5,8 +5,7 @@ docker pull a32111666/nginx2
 kubectl run k8s --image=32111666a/nginx2 --port=80
 
 
-kubectl expose k8s/ngix2 --type="NodePort" --port 80 32111666a/nginx2 exposed
-
+kubectl expose deployment/k8s --type="NodePort" --port 80 
      
 kubectl scale deployment k8s --replicas=20
 
