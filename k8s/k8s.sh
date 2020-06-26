677ea27ef221494470644aede190a4af2f9c8551
mingdiii <64004683+mingdiii@users.noreply.github.com>
2020-06-26 17:02:30 +0900

    Update k8s.sh

--- k8s/k8s.sh
+++ k8s/k8s.sh
@@ -8,6 +8,10 @@ docker pull a32111666/nginx2
 # container 
 kubectl run k8s --image=32111666a/nginx2 --port=80
 
+# expose
+kubectl expose deployment/nginx --type="NodePort" --port 80 32111666a/nginx2
+
+
 # 20 port 
 kubectl scale deployment k8s --replicas=20
 
